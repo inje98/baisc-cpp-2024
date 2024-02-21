bbef662697f4e84960e175193e1613b14505c361
inje98 <kij980723@gmail.com>
2024-02-21 13:29:22 +0900

    주석빼고 다시

--- day01/Project1/Project1/test02.cpp
+++ day01/Project1/Project1/test02.cpp
@@ -1,12 +1,12 @@
-//# include<iostream>
-//
-//int main(void) 
-//{
-//	int num = 20;
-//	std::cout << "Hello World!" << std::endl; // 문자열은 큰따옴표, 문자는 작은따옴표
-//	std::cout << "Hello " << "World!" << std::endl;
-//	std::cout << num << ' ' << 'A';
-//	std::cout << ' ' << 3.14 << std::endl;
-//
-//	return 0;
-//}
+# include<iostream>
+
+int main(void) 
+{
+	int num = 20;
+	std::cout << "Hello World!" << std::endl; // 문자열은 큰따옴표, 문자는 작은따옴표
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	return 0;
+}
