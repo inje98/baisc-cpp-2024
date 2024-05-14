fefb19226c2de0ea6182223fb683e8332893cdda
inje98 <kij980723@gmail.com>
2024-05-14 09:32:37 +0900

    정리

---
+++ day13/dbo.Book.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [bookid]    INT          NOT NULL,
+    [bookname]  VARCHAR (40) NULL,
+    [publisher] VARCHAR (40) NULL,
+    [price]     INT          NULL,
+    PRIMARY KEY CLUSTERED ([bookid] ASC)
+);
+
